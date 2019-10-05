e5ef3ae2231bce50d40ff07e48891c49aa0ed671
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2019-10-05 16:18:36 -0700

    made lots n lots of changes

--- IN DEVELOPMENT/MTM.js
+++
@@ -1,8 +0,0 @@
-function openMember(memberName) {
-  var i;
-  var x = document.getElementsByClassName("member");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(memberName).style.display = "block";
-}
